08dd7e9e378d41af35ca70dd2620e74fb495e45a
fernandapadilha <vitoriafernandaifc@gmail.com>
2024-07-17 09:06:19 -0300

    first commit

---
+++ src/views/EquipeView.vue
@@ -0,0 +1,40 @@
+<script>
+    export default {
+        name: 'EquipeView',
+        data() {
+            return {
+                equipe: [
+                    {
+                        nome: 'Renata',
+                        cargo: 'Desenvolvedor'
+                    },
+                    {
+                        nome: 'Vinícius',
+                        cargo: 'Designer'
+                    },
+                    {
+                        nome: 'Vitória',
+                        cargo: 'Gerente de Projetos'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+
+<template>
+    <div>
+        <h1>Equipe</h1>
+        <ul>
+            <li v-for="membro in equipe" :key="membro.nome">
+                <h2>{{ membro.nome }}</h2>
+                <p>{{ membro.cargo }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+/* Your component-specific styles go here */
+</style>
